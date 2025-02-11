c8a9afc21b6fb4412adebc781b2448eebb8e4333
Daniel Saravia <108732138+Daniel-Saravia@users.noreply.github.com>
2025-02-11 11:36:09 -0700

    Update feb11.sql

--- feb11.sql
+++ feb11.sql
@@ -14,3 +14,9 @@ N'{
     },
     "type":"Basic"
 }'
+SELECT 
+    JSON_VALUE(@json, '$.type') as type,
+    JSON_VALUE(@json, '$.info.address.town') as town,
+    JSON_QUERY(@json, '$.info.tags') as tags
+SELECT value
+FROM OPENJSON(@json, '$.info.tags')
